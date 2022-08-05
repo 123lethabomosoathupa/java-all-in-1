da97e54f6512793f7484b03d847a29234d6ea2c4
123lethabomosoathupa <99099883+123lethabomosoathupa@users.noreply.github.com>
2022-08-05 11:47:46 +0200

    book one done

---
+++ Book 1/HelloApp/src/helloapp/HelloApp.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloapp;
+
+/**
+ *
+ * @author letha
+ */
+public class HelloApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        printHello("World!");
+    }
+    
+    public static void printHello(String greetee) {
+        System.out.println("Hello " + greetee);
+        
+    }
+    
+}
