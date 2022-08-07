12e42aa2ac81340de6f322d3898099f7f3369bad
123lethabomosoathupa <99099883+123lethabomosoathupa@users.noreply.github.com>
2022-08-07 11:33:42 +0200

    Chapter2 done

---
+++ Book 2/CaseApp/src/caseapp/CaseApp.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package caseapp;
+
+/**
+ *
+ * @author letha
+ */
+public class CaseApp {
+
+    public static void main(String[] args) {
+        For(int int i = 0;
+i< 5; i++)
+ System.out.println("Hi");
+    }
+}
