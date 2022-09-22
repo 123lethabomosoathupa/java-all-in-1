551750866afbf0722660dc83ee9ce2c8f3d6e0d6
123lethabomosoathupa <99099883+123lethabomosoathupa@users.noreply.github.com>
2022-09-22 13:56:33 +0200

    Update

--- Book5/Chapter 1/CountDownClock/src/countdownclock/CountDownApp.java
+++ Book5/Chapter 1/CountDownClock/src/countdownclock/CountDownApp.java
@@ -11,7 +11,7 @@ package countdownclock;
 public class CountDownApp {
 
     public static void main(String[] args) {
-        Thread clock = new CountDownClock();
-        clock.start();
+        Thread clock = new CountDownClock();        //calls for CountDownClock class
+        clock.start();                              //calls the start method
     }
 }
--- Book5/Chapter 1/CountDownClock/src/countdownclock/CountDownClock.java
+++ Book5/Chapter 1/CountDownClock/src/countdownclock/CountDownClock.java
@@ -4,19 +4,20 @@
  */
 package countdownclock;
 
-/**
- *
- * @author letha
- */
+//In a multithreaded Java application, you can create an object that can be run as a thread by using the Thread class.
+
+//The following program can be found as an example of a Count Down Process used by either NASA or SpaceX
+
+
 public class CountDownClock extends Thread                 //class extends the Thread class	
 {
 
-    public void run() {                                    //method is called by Java when the clock thread has been started
+    public void run() {                                    //Java calls a function after launching the clock thread.
         for (int t = 20; t >= 0; t--) {                    //for loop counts down from 20 to 0
             System.out.println("T minus " + t);           
             try {
-                Thread.sleep(1000);                       //The CountDownClock class uses the sleep method to pause for 1 second.
-            } catch (InterruptedException e) {            //handles exceptions
+                Thread.sleep(1000);                       //The sleep method is used by the CountDownClock class to pause for one second.
+            } catch (InterruptedException e) {            //InterruptedException handles exceptions
             }
         }
     }
