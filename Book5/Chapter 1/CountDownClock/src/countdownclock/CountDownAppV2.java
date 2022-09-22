5a878d4916bce44899ec445b492cd52abdbf345f
123lethabomosoathupa <99099883+123lethabomosoathupa@users.noreply.github.com>
2022-09-22 14:33:17 +0200

    Update

--- Book5/Chapter 1/CountDownClock/src/countdownclock/CountDownAppV2.java
+++ Book5/Chapter 1/CountDownClock/src/countdownclock/CountDownAppV2.java
@@ -13,14 +13,14 @@ import java.util.ArrayList;
 public class CountDownAppV2 {
 
     public static void main(String[] args) {
-        Thread clock = new CountDownClock();
-        ArrayList<Runnable> events
-                = new ArrayList<Runnable>();
+        Thread clock = new CountDownClock();                        //The Count DownClock class instance is created at the beginning of the main method and saved in the clock variable.    
+        ArrayList<Runnable> events = new ArrayList<Runnable>();     //The next step is to create three LaunchEvent objects, which are timed.
+                                                                    //Runnable variables with the names flood, ignition, and liftoff are given these objects.
         events.add(new LaunchEvent(16, "Flood the pad!"));
         events.add(new LaunchEvent(6, "Start engines!"));
         events.add(new LaunchEvent(0, "Liftoff!"));
         clock.start();
-        for (Runnable e : events) {
+        for (Runnable e : events) {                                 //For loop ensures objects are executed
             new Thread(e).start();
         }
     }
