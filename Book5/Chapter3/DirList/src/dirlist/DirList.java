1dd1b276e437006bc20c22706b743fa021a8b4ba
123lethabomosoathupa <99099883+123lethabomosoathupa@users.noreply.github.com>
2022-09-29 13:40:36 +0200

    Update

--- Book5/Chapter3/DirList/src/dirlist/DirList.java
+++ Book5/Chapter3/DirList/src/dirlist/DirList.java
@@ -8,9 +8,7 @@ package dirlist;
  *
  * @author letha
  */
-import java.io.File;
-
-
+import java.io.File;                                     //Required to be bale to use the file class
 import java.util.Scanner;
 
 public class DirList {
@@ -21,59 +19,37 @@ public class DirList {
         System.out.print(
                 "Welcome to the Directory Lister");
         do {
-            System.out.print("\nEnter a path: ");
+            System.out.print("\nEnter a path: ");          //A Scanner object is used to get the pathname from the user
             String path = sc.nextLine();
 
             File dir = new File(path);
-           
- if (!dir.exists() || !dir.isDirectory()) {
-                
+
+            if (!dir.exists() || !dir.isDirectory()) {
+                System.out.println(
+                        "\nThat directory doesn't exist.");
+            } else {
+                System.out.println(
+                        "\nListing directory tree of:");
+                System.out.println(dir.getPath());
+                listDirectories(dir, "  ");
             }
-            
- System.out.println(
-                    "\nThat directory doesn't exist.");
-            else
- {
-                        System.out.println(
-                                "\nListing directory tree of:");
-                        System.out.println(dir.getPath());
-                        
- listDirectories(dir, " "); 
- }
         } while (askAgain());
-    
+        //and the loop repeats if the user answers Y.
+    }
 
-    
- }
- private static void listDirectories(
- File dir, String indent) {
+    private static void listDirectories(
+            File dir, String indent) {
         File[] dirs = dir.listFiles();
-        
- for (File f : dirs) {
-            
-        }
-        
-            
- {
- if (f.isDirectory()) {
-                
-            }
-            
-                
- {
- System.out.println(
+        for (File f : dirs) {
+            if (f.isDirectory()) {
+                System.out.println(
                         indent + f.getName());
-                
- listDirectories(f, indent + " ");
-            
-        
-    
+                listDirectories(f, indent + "  ");
+            }
+        }
+    }
 
-    
- }
- }
- }
- private static boolean askAgain() {
+    private static boolean askAgain() {
         System.out.print("Another? (Y or N) ");
         String reply = sc.nextLine();
         if (reply.equalsIgnoreCase("Y")) {
