43a5f63a4d33693ccb0a299a088e032696712d04
123lethabomosoathupa <99099883+123lethabomosoathupa@users.noreply.github.com>
2022-10-11 23:30:55 +0200

    Book5 Finished

---
+++ Book5/Chap4/FormatDateTime/src/formatdatetime/FormatDateTime.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package formatdatetime;
+
+/**
+ *
+ * @author letha
+ */
+import java.util.*;
+import java.time.*;
+import java.time.format.*;
+
+public class FormatDateTime {
+
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        printDate(now, "YYYY-MM-dd");
+        printDate(now, "MM-dd-YYYY");
+        printDate(now, "dd MMM YYYY");
+        printDate(now, "MMMM d, YYYY");
+        printDate(now, "HH:mm");
+        printDate(now, "h:mm a");
+    }
+
+    public static void printDate(LocalDateTime date, String pattern) {
+        DateTimeFormatter f;
+        f = DateTimeFormatter.ofPattern(pattern);
+        pattern = (pattern + " ");
+        System.out.println(pattern + " " + date.format(f));
+    }
+}
